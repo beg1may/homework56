0fcf2102987427a98a1325812682978df5956b17
Silalieva Begimay <begimayomurovna@gmail.com>
2024-01-13 10:04:52 +0600

    Добавлены фотографии ингредиентов

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BurgerBuilder from './IngredientButtons';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <BurgerBuilder />
+        </div>
+    );
+};
+
+export default App;
